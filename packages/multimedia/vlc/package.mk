9f3119e9557b95d159d2346fb7cdb24f4c1bb6c1
navy1978 <ivan.morelli@gmail.com>
2026-09-05 06:13:05 +0000

    build: support native ARM64 build hosts

--- packages/multimedia/vlc/package.mk
+++ packages/multimedia/vlc/package.mk
@@ -8,7 +8,7 @@ PKG_VERSION="3.0.23"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.videolan.org"
 PKG_URL="https://mirror.netcologne.de/videolan.org/${PKG_NAME}/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain gnutls ffmpeg libmpeg2 zlib flac libvorbis libxml2 pulseaudio SDL2 x264 aom libogg librga ${OPENGLES}"
+PKG_DEPENDS_TARGET="toolchain gnutls ffmpeg libmpeg2 zlib flac libvorbis libxml2 pulseaudio SDL2 x264 aom libogg libjpeg-turbo librga ${OPENGLES}"
 PKG_SHORTDESC="VideoLAN multimedia player and streamer"
 PKG_LONGDESC="VLC is the VideoLAN project's media player. It plays MPEG, MPEG2, MPEG4, DivX, MOV, WMV, QuickTime, mp3, Ogg/Vorbis files, DVDs, VCDs, and multimedia streams from various network sources."
 
